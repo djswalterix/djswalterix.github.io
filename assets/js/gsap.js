be02f66a9b2c1e5402faa32fbf7c2916275d5514
djswalterix <walter45hoxha@gmail.com>
2023-12-05 17:57:58 +0100

    projects bug

--- assets/js/gsap.js
+++ assets/js/gsap.js
@@ -39,3 +39,62 @@ document.addEventListener("DOMContentLoaded", function () {
     duration: 1,
   });
 });
+
+// Opzione di animazione comune
+const techAnimation = {
+  opacity: 1,
+  y: 0,
+  duration: 1,
+  ease: "power1.out",
+};
+
+// Anima ogni elemento tecnologico individualmente
+gsap.to("#html-css", { ...techAnimation, scrollTrigger: "#html-css" });
+gsap.to("#javascript", {
+  ...techAnimation,
+  scrollTrigger: "#javascript",
+  delay: 0.2,
+});
+gsap.to("#node", { ...techAnimation, scrollTrigger: "#node", delay: 0.4 });
+gsap.to("#react", { ...techAnimation, scrollTrigger: "#react", delay: 0.6 });
+gsap.to("#mysql", { ...techAnimation, scrollTrigger: "#mysql", delay: 0.8 });
+/*
+document.querySelectorAll(".tech").forEach((tech) => {
+  tech.addEventListener("mouseenter", () => {
+    // Inizia l'animazione quando il mouse entra sull'elemento
+    gsap.to(tech, { scale: 1.1, duration: 0.3, ease: "power1.out" });
+  });
+
+  tech.addEventListener("mouseleave", () => {
+    // Ritorna allo stato originale quando il mouse lascia l'elemento
+    gsap.to(tech, { scale: 1, duration: 0.3, ease: "power1.out" });
+  });
+});
+*/
+document.querySelectorAll(".tech").forEach((tech) => {
+  let hoverTween = gsap.to(tech, {
+    scale: 1.1,
+    duration: 0.3,
+    paused: true,
+    ease: "power1.out",
+    transformOrigin: "center center", // Mantiene l'animazione centrata
+  });
+
+  tech.addEventListener("mouseenter", () => {
+    hoverTween.play();
+  });
+
+  tech.addEventListener("mouseleave", () => {
+    hoverTween.reverse();
+  });
+});
+
+document.querySelectorAll(".project-card").forEach((card) => {
+  gsap.from(card, {
+    scrollTrigger: card,
+    duration: 0.5,
+    opacity: 0,
+    y: 50,
+    stagger: 0.2, // Per un effetto a cascata se hai più card
+  });
+});
---
+++ assets/js/scrollify.js
@@ -0,0 +1,9 @@
+$(function () {
+  $.scrollify({
+    section: ".section",
+
+    //setHeights: false, // Imposta su false se le altezze sono definite nel CSS
+    scrollSpeed: 800,
+    // updateHash: false, // Imposta su true se vuoi aggiornare l'hash URL durante lo scorrimento
+  });
+});
